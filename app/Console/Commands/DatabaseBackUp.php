04dfc2344d3ddfcb7b55eecaf759ab357af6dcf2
ZumaAkbarID <rahmatwahyumaakbar@gmail.com>
2024-03-07 00:25:30 +0700

    fix: missing bayar kas, feat: change monthly ledger, auto backup db

--- app/Console/Commands/DatabaseBackUp.php
+++ app/Console/Commands/DatabaseBackUp.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Carbon\Carbon;
+use CURLFile;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
@@ -36,6 +37,35 @@ class DatabaseBackUp extends Command
         $output  = NULL;
 
         exec($command, $output, $returnVar);
-        Log::info("Created Backup DB : " . $path);
+
+        $CHAT_ID = '894168042';
+        $BOT = '6664534491:AAEkDMfKRcpQoC7jObA2f0Vf_4XNwi3bz3o';
+
+        // Create CURL object
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot" . $BOT . "/sendDocument?chat_id=" . $CHAT_ID);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+        // Create CURLFile
+        $finfo = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $path);
+        $cFile = new CURLFile($path, $finfo);
+
+        // Add CURLFile to CURL request
+        curl_setopt($ch, CURLOPT_POSTFIELDS, [
+            "document" => $cFile,
+            "caption" => "Backup DB Kontrakan"
+        ]);
+
+        // Call
+        $result = json_decode(curl_exec($ch), true);
+
+        // Show result and close curl
+        // var_dump($result);
+        curl_close($ch);
+
+        if ($result['ok'] == true) {
+            unlink($path);
+        }
     }
 }
